8118ee4c4e265306cc1d87a4310dad026687b74b
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-19 14:22:47 +0100

    Finished task 1

---
+++ hello_world.js
@@ -0,0 +1,13 @@
+let myName = "Percy";
+const randomNumber = Math.random();
+let x
+x = randomNumber;
+let age;
+age = Math.round (100 * x);
+console.log (age);
+if (age === 22) {
+  alert (`My name is ${myName} and I am ${age} years old.`);
+}
+else {
+  alert(`My name is ${myName} and I am not ${age} years old.`);
+}
