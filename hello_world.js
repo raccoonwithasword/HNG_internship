e0278bd60363e7d0027596bc8b7431ac02372c74
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-19 15:47:56 +0100

    l

--- hello_world.js
+++ hello_world.js
@@ -7,7 +7,6 @@ age = Math.round (100 * x);
 console.log (age);
 if (age === 22) {
   alert (`My name is ${myName} and I am ${age} years old.`);
-}
 else {
   alert(`My name is ${myName} and I am not ${age} years old.`);
 }
