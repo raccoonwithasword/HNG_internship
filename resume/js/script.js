e149bbf001a8eddc56c6e8ad32de2bc6372bb91d
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-20 17:07:58 +0100

    Fun facts about me

---
+++ resume/js/script.js
@@ -0,0 +1,8 @@
+const facts_me = [
+    "I don't snort cocaine.",
+    "My favorite book is 'Children of Blood and Bone' by Tomi Adeyemi.",
+    "I am currently walking the line between Nihilism and Stoicism.",
+    "If we've met before, no we haven't.",
+    "I am hungry at the moment (I'm constantly hungry, so this is a given).",
+    "My favorite lizard (for now) is the green basilisk lizard because they can run on water"
+]
