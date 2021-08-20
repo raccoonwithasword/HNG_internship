4da91752287aebac13e127acf1d4eb58f6130c22
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-20 18:06:42 +0100

    Added alt txt & animal facts

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,8 +1,19 @@
-const facts_me = [
+const facts_me  = [
     "I don't snort cocaine.",
     "My favorite book is 'Children of Blood and Bone' by Tomi Adeyemi.",
     "I am currently walking the line between Nihilism and Stoicism.",
     "If we've met before, no we haven't.",
     "I am hungry at the moment (I'm constantly hungry, so this is a given).",
-    "My favorite lizard (for now) is the green basilisk lizard because they can run on water"
+    "My favorite lizard (for now) is the green basilisk lizard because they can run on water."
+]
+const facts_animal = [
+    "The green basilisk lizard can run on water.",
+    "Koalas can't recogize food that isn't on the tree.",
+    "Koalas eat eucalyptus, which is poisonous. Because they're dumb.",
+    "The now extinct giant ground sloths are the reason we have avocados today. They would eat the whole fruit whole and poop out the seeds.",
+    "There is a species of jellyfish that is immortal.",
+    "Ostriches can run faster than horses.",
+    "The Australian military once fought a war against emus. And lost.",
+    "Koalas have fingerprints that are very similar to human fingerprints.",
+    "A snail can sleep for three years."
 ]
