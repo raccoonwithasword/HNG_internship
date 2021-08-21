446d91e34046c09e228f90a0a53da00dfd851dc8
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-21 19:16:26 +0100

    ok

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,7 +1,19 @@
 
 function hide() {
     document.getElementById("click");
+    if (click.style.display === "none") {
        click.style.display = "block";
+    } else {
+        click.style.display ="none";
+    }
+}
+function change() {
+    document.getElementById("surprise");
+    if (surprise.innerHTML === "Hit me again!") {
+        surprise.innerHTML = "Hit me!";
+    } else {
+        surprise.innerHTML = "Hit me again!";
+    }
 }
 
 
